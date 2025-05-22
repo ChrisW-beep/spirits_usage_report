9cf957601fc0fd5599bcf728121095ac41d3a2b2
Chris Wilson <cwilson@posnation.com>
2025-05-22 11:23:17 -0400

    Update generate_store_summary_s3.py

--- generate_store_summary_s3.py
+++ generate_store_summary_s3.py
@@ -17,7 +17,8 @@ summary_rows = []
 # === HELPERS ===
 def read_csv_from_s3(key):
     obj = s3.get_object(Bucket=BUCKET, Key=key)
-    return pd.read_csv(obj['Body'])
+    return pd.read_csv(obj['Body'], low_memory=False)
+
 
 def read_ini_from_s3(key):
     obj = s3.get_object(Bucket=BUCKET, Key=key)
